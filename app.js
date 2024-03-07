22dd0c8d65e6a06b93e7b7eb1f3a970de7b9f302
george <g.prokhorov@chnu.edu.ua>
2024-03-07 20:39:48 +0200

    2step

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import items from './data/items.json';
+import _ from 'lodash';
 
 
 const PORT = 3000;
 const server = express();
+const BASE_ITEMS_URL = '/api/v1/items';
 
 console.log("Hello JS");
 const message = "Hello Babel !!!!!!!!!";
@@ -14,3 +17,18 @@ server.listen(PORT, () => {
 server.get('/', (req, res) =>{
    res.send("Hello from Express!!!");
 });
+
+server.get(BASE_ITEMS_URL, (req, res) => {
+    res.json(items);
+});
+
+server.get(BASE_ITEMS_URL+'/:id', (req, res) => {
+    const id = req.params.id;
+    const item = _.find(items, item => item.id === id);
+    if (item) {
+        res.json(item);
+    } else {
+        res.send('Not found');
+    }
+    }
+);
