bf96bad99ccf0116a10a23b00d434670da22ce06
george <g.prokhorov@chnu.edu.ua>
2024-03-07 19:55:50 +0200

    1step

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const server = express();
 console.log("Hello JS");
 const message = "Hello Babel !!!!!!!!!";
 console.log(message);
-
+      
 server.listen(PORT, () => {
     console.log(`Server has been started on port ${PORT}`)
 });
